e5c18c276db75e4698d288b49608d2130663eb9d
Dom <domeccleston@gmail.com>
2019-08-19 14:37:28 +0100

    finished assignment

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -117,11 +117,11 @@ let BMWAndAudi = [];
 
 // using for loop:
 
-/* for (let i = 0; i < inventory.length; i++) {
+ for (let i = 0; i < inventory.length; i++) {
   if (inventory[i].car_make === "BMW" || inventory[i].car_make === "Audi") {
     BMWAndAudi.push(inventory[i]);
   }
-} */
+} 
 
 
 
