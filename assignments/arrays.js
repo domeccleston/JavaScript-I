32d4ca3ed3b0f5506fa1f3b8b0f9381c76c25902
Dom <domeccleston@gmail.com>
2019-08-19 21:47:19 +0100

    finished fat arrow functions in arrays.js

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -125,15 +125,17 @@ console.log(oldCars.length);
 // A buyer is interested in seeing only BMW and Audi cars within the inventory. Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 
 // using for loop:
-/* let BMWAndAudi = [];
+
+/*  let BMWAndAudi = [];
  for (let i = 0; i < inventory.length; i++) {
   if (inventory[i].car_make === "BMW" || inventory[i].car_make === "Audi") {
     BMWAndAudi.push(inventory[i]);
   }
-}  */
+}   */
 
- let BMWAndAudi = inventory.filter(car => inventory[car].car_make === 'BMW' || inventory[car].car_make === 'Audi');
+// using .filter(): 
 
+let BMWAndAudi = inventory.filter(car => car.car_make === "Audi" || car.car_make === "BMW");
 
-
-console.log(JSON.stringify(BMWAndAudi));
+console.log(JSON.stringify(BMWAndAudi))
+ 
