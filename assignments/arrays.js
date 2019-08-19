3b0f3b9854d4fdd15db653233328d2c0ac418c99
Dom <domeccleston@gmail.com>
2019-08-19 21:26:36 +0100

    fixed filter() solution of arrays.js challenge 5

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -101,28 +101,31 @@ console.log(carYears);
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
-// solution using loop:
+
+// Solution using loop:
+
 /* let oldCars = [];
   for (let i = 0; i < carYears.length; i++) {
   carYears[i] < 2000 ? oldCars.push(carYears[i]) : 0;
 } */
-// solution using filter():
-let oldCars = carYears.filter(year => carYears[year] < 2000); 
+
+// Solution using array.filter():
+let oldCars = carYears.filter(year => year < 2000); 
 
 console.log(oldCars.length);
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory. Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
-let BMWAndAudi = [];
 
 // using for loop:
-
+/* let BMWAndAudi = [];
  for (let i = 0; i < inventory.length; i++) {
   if (inventory[i].car_make === "BMW" || inventory[i].car_make === "Audi") {
     BMWAndAudi.push(inventory[i]);
   }
-} 
+}  */
 
+ let BMWAndAudi = inventory.filter(car => inventory[car].car_make === 'BMW' || inventory[car].car_make === 'Audi');
 
 
 
